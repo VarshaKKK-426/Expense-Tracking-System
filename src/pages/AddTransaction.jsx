0c742c3e75f7c2da63d9c012ba72b296e239a118
Varsha K <varshasuresh426@gmail.com>
2025-11-10 18:05:55 +0530

    add transaction logic done

--- src/pages/AddTransaction.jsx
+++ src/pages/AddTransaction.jsx
@@ -8,9 +8,34 @@ function AddTransaction() {
     const [description, setDescription] = useState("");
     const [date, setDate] = useState("");
 
-    function submitForm() {
-        console.log(type, amount, category, description, date);
+    function addTransaction() {
+        if (!amount || !category || !description || !date) {
+            alert("Please fill all the fields");
+            return
+        }
+
+        const existingTransactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+        const currentTransaction = {
+            type: type,
+            amount: amount,
+            category: category,
+            description: description,
+            date: date
+        };
+
+        const newTransaction = [...existingTransactions, currentTransaction]
+
+        localStorage.setItem("transactions", JSON.stringify(newTransaction));
+        
+        setType("Expense");
+        setAmount("");
+        setCategory("");
+        setDescription("");
+        setDate("");
+
     }
+
     return (
         <div className="addTransactionContainer">
             <h3>Add New Transaction</h3>
@@ -24,7 +49,7 @@ function AddTransaction() {
                     </label>
                 </div>
                 <input type="text" placeholder="Enter Amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
-                <select onChange={(e) => setCategory(e.target.value)}>
+                <select onChange={(e) => setCategory(e.target.value)} value={category}>
                     <option value="">Select a Category</option>
                     <option value="Salary">Salary</option>
                     <option value="Groceries">Groceries</option>
@@ -33,9 +58,9 @@ function AddTransaction() {
                     <option value="Entertainment">Entertainment</option>
                     <option value="Others">Others</option>
                 </select>
-                <textarea placeholder="Description" onChange={(e) => setDescription(e.target.value)}></textarea>
-                <input type="date" onChange={(e) => setDate(e.target.value)} />
-                <button onClick={submitForm}>Add Transaction</button>
+                <textarea placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+                <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+                <button onClick={addTransaction}>Add Transaction</button>
             </div>
         </div>
     );
