3317b809c44ccd1a7ec1b5a1e2df1b7936c4f4bf
Varsha K <varshasuresh426@gmail.com>
2025-11-11 11:33:19 +0530

    Display Transactions

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,7 +1,37 @@
+import React from "react";
+import "../styles/Transactions.css";
+
 function Transactions() {
+    const existingTransactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
     return (
-        <div>
-            <h1>Welcome to the Transactions</h1>
+        <div className="transactions-container">
+            <h3>All Transactions</h3>
+            <table className="transactionTable">
+                <thead>
+                    <tr>
+                        <th>Category</th>
+                        <th>Type</th>
+                        <th>Amount</th> 
+                        <th>Description</th>
+                        <th>Date</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {existingTransactions.map((transaction, index) => (
+                        <tr key={index}>
+                            <td>{transaction.category}</td>
+                            <td>{transaction.type}</td>
+                            <td className={transaction.type === 'Income' ? 'income' : 'expense'}>{transaction.amount}</td>
+                            <td>{transaction.description || 'No description'}</td>
+                            <td>{transaction.date}</td>
+                        </tr>
+                    ))}
+
+                </tbody>
+            </table>
+
         </div>
     );
 }
